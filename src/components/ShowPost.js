13124aefebffa9452bf9fb088cdec1f3d936c799
Shireen Garcia <garciasal@gmail.com>
2017-08-31 17:32:47 -0700

    Connecting routes

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ShowPost extends Component {
   render() {
     return (
-      <p>Hello from the showpost</p>
+      <div>
+        <Link to="/postlist">
+          <button type="button" className="btn btn-primary">Back To Blog List</button>
+        </Link>
+      </div>
     );
   }
 }
